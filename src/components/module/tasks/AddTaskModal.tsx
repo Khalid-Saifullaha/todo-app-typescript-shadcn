e2946019e29ddbf1a69bfefa4c214a2ca410665d
Your Name <khalidsaifullaha3@gmail.com>
2025-08-19 21:49:55 +0600

    Add Task Modal added

---
+++ src/components/module/tasks/AddTaskModal.tsx
@@ -0,0 +1,71 @@
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { DialogDescription } from "@radix-ui/react-dialog";
+import { useForm } from "react-hook-form";
+
+export function AddTaskModal() {
+  const form = useForm({
+    defaultValues: {
+      title: "",
+    },
+  });
+
+  function onSubmit(values: any) {
+    console.log("Task added:", values);
+  }
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button>Add Task</Button>
+      </DialogTrigger>
+
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogDescription className="sr-only">
+          Fill up this form to add task
+        </DialogDescription>
+        <DialogHeader>
+          <DialogTitle>Add Task</DialogTitle>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-5">
+            <FormField
+              control={form.control}
+              name="title"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Title</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Enter task title" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <DialogFooter>
+              <Button type="submit">Save Task</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+}
