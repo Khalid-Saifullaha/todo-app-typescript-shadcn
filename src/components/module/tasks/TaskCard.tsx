265a1025981aae69bc4deb6e3b7d88d0df8dafb2
Your Name <khalidsaifullaha3@gmail.com>
2025-08-20 12:18:53 +0600

    delete and checkbox make clickable

--- src/components/module/tasks/TaskCard.tsx
+++ src/components/module/tasks/TaskCard.tsx
@@ -1,19 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
 import { ITask } from "@/types";
 import { Button } from "@/components/ui/button";
-import { Trash2, Check } from "lucide-react";
-import * as Checkbox from "@radix-ui/react-checkbox";
+import { Trash2 } from "lucide-react";
 import { cn } from "@/lib/utils";
+import { useAppDispatch } from "@/redux/hook";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  deleteTask,
+  toggleCompleteState,
+} from "@/redux/features/task/taskSlice";
 
 interface TaskCardProps {
   task: ITask;
 }
 
 const TaskCard = ({ task }: TaskCardProps) => {
-  const [isCompleted, setIsCompleted] = useState(task.isCompleted || false);
+  const dispatch = useAppDispatch();
 
   return (
-    <div className="border px-3 py-3 rounded-md   mt-5 shadow-md">
+    <div className="border px-3 py-3 rounded-md mt-5 shadow-md">
       <div className="flex justify-between items-center">
         <div className="flex gap-2 items-center">
           <div
@@ -23,23 +28,25 @@ const TaskCard = ({ task }: TaskCardProps) => {
               "bg-red-500": task.priority === "High",
             })}
           ></div>
-          <h1>{task.title}</h1>
+          <h1 className={cn({ "line-through": task.isCompleted })}>
+            {task.title}
+          </h1>
         </div>
         <div className="flex gap-3 items-center">
-          <Button variant="link" className="p-0 text-red-500">
+          <Button
+            onClick={() => dispatch(deleteTask(task.id))}
+            variant="link"
+            className="p-0 text-red-500"
+          >
             <Trash2 />
           </Button>
 
-          {/* Radix Checkbox */}
-          <Checkbox.Root
-            checked={isCompleted}
-            onCheckedChange={(checked) => setIsCompleted(!!checked)}
-            className="w-5 h-5 border rounded flex items-center justify-center"
-          >
-            <Checkbox.Indicator>
-              <Check className="w-4 h-4 text-green-500" />
-            </Checkbox.Indicator>
-          </Checkbox.Root>
+          <Checkbox
+            checked={task.isCompleted}
+            onCheckedChange={() => {
+              dispatch(toggleCompleteState(task.id));
+            }}
+          />
         </div>
       </div>
       <p className="mt-5">{task.description}</p>
--- src/redux/features/task/taskSlice.ts
+++ src/redux/features/task/taskSlice.ts
@@ -13,7 +13,16 @@ const createTask = (taskData: DraftTask): ITask => {
   return { id: nanoid(), isCompleted: false, ...taskData };
 };
 const initialState: InitialState = {
-  tasks: [],
+  tasks: [
+    {
+      id: "bnm1",
+      title: "Testing",
+      description: "Write unit and integration tests",
+      dueDate: "2025-08-30",
+      isCompleted: false,
+      priority: "High",
+    },
+  ],
   filter: "all",
 };
 const taskSlice = createSlice({
@@ -23,6 +32,17 @@ const taskSlice = createSlice({
     addTask: (state, action: PayloadAction<DraftTask>) => {
       const taskData = createTask(action.payload);
       state.tasks.push(taskData);
+    },
+    toggleCompleteState: (state, action: PayloadAction<string>) => {
+      console.log(action);
+      state.tasks.forEach((task) =>
+        task.id == action.payload
+          ? (task.isCompleted = !task.isCompleted)
+          : task
+      );
+    },
+    deleteTask: (state, action: PayloadAction<string>) => {
+      state.tasks = state.tasks.filter((task) => task.id !== action.payload);
     },
   },
 });
@@ -34,6 +54,6 @@ export const selectFilter = (state: RootState) => {
   return state.todo.filter;
 };
 
-export const { addTask } = taskSlice.actions;
+export const { addTask, toggleCompleteState, deleteTask } = taskSlice.actions;
 
 export default taskSlice.reducer;
